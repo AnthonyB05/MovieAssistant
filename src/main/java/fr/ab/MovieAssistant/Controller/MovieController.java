9cb081644510093096c039911f5de514339510cd
AnthonyB05 <77343444+AnthonyB05@users.noreply.github.com>
2023-02-09 15:40:08 +0100

    add error.html + post request

--- src/main/java/fr/ab/MovieAssistant/Controller/MovieController.java
+++ src/main/java/fr/ab/MovieAssistant/Controller/MovieController.java
@@ -4,6 +4,7 @@ import fr.ab.MovieAssistant.Service.MovieService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,6 +18,11 @@ public class MovieController {
     @GetMapping("/hello")
     public ResponseEntity<String> getHello() {
         return ResponseEntity.ok("hello");
+    }
+
+    @PostMapping("/request")
+    public ResponseEntity<String> postRequest() {
+        return ResponseEntity.ok("request");
     }
 
 }
